1b1cd1cbcf3ad339345457d3c51ddbbd57d1ff8d
hariwij <hariwijesinghe234@gmail.com>
2022-01-19 20:21:54 +0530

    Welcome Ui Done

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1>My Library</h1>
+      <div className="imgContainer"></div>
+    </div>
+  );
+};
+
+export default Welcome;
