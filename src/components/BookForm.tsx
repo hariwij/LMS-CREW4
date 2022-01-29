7d3c5d745f23383aeda081380bfa29ba878fca5f
pinsaradhanika97 <pinsaradhanika@gmail.com>
2022-01-29 08:41:37 +0530

    Book form ui/ux done. Need to fix the button color

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,8 +1,28 @@
-import { Container } from "react-bootstrap";
+import {Container, Form} from "react-bootstrap";
+import React from "react";
+import '../styles/components/_BookForm.scss'
+import { Trash2,Plus,Edit,XCircle} from 'react-feather';
 
 const BookForm : React.FC = () => {
     return(
-        <h1>Create Book</h1>
+        <Form className='dataFormContainer'>
+            <div className='formHeading'>
+                <h2 >Create Book</h2>
+                <XCircle/>
+            </div>
+            <label>Title of the book
+                <input type='text' name='title'/>
+            </label>
+            <label>ISBN
+                <input type='text' name='isbn'/>
+            </label>
+            <label>Author
+                <input type='text' name='author'/>
+            </label>
+            <button className='btn-primary'> Submit </button>
+
+        </Form>
+
     )
 }
 
