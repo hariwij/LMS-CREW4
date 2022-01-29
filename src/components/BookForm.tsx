ff49bf51208e8a97c065c22936a802675cfbafa4
pinsaradhanika97 <pinsaradhanika@gmail.com>
2022-01-29 09:44:55 +0530

    select field under-fix state commit

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,14 +1,21 @@
 import {Container, Form} from "react-bootstrap";
-import React from "react";
+// import React from "react";
 import '../styles/components/_BookForm.scss'
 import { Trash2,Plus,Edit,XCircle} from 'react-feather';
+import Select from "react-select/base";
+import React, { Component } from 'react';
 
 const BookForm : React.FC = () => {
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ];
     return(
         <Form className='dataFormContainer'>
             <div className='formHeading'>
                 <h2 >Create Book</h2>
-                <XCircle/>
+                <XCircle style={{width:'2rem',height:'2rem'}}/>
             </div>
             <label>Title of the book
                 <input type='text' name='title'/>
@@ -17,9 +24,10 @@ const BookForm : React.FC = () => {
                 <input type='text' name='isbn'/>
             </label>
             <label>Author
+                {/*<Select options={options} />*/}
                 <input type='text' name='author'/>
             </label>
-            <button className='btn-primary'> Submit </button>
+            <button className='btn-primary'> Create </button>
 
         </Form>
 
